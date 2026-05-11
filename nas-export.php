ea3a70d7653d4ed47ad8ff63a021274e91a9fb5a
Frank Reichert <31617868+iccander@users.noreply.github.com>
2026-05-11 16:21:09 +0200

    Laufrichtung
    1.) Umkehrung der Laufrichtung: GEOgraf-out spaziert nämlich bei Polygonen munter rechtsherum. In GML-Geometrien (wie ALKIS) ist es aber genau andersherum und die Koordinaten von Umringspolygonen werden in der Regel gegen den Uhrzeigersinn geführt.
    2.) Dateiname mit Umlauten/Sonderzeichen in der Antragsnummer

--- nas-export.php
+++ nas-export.php
@@ -1,5 +1,6 @@
+3396178
 <?php
-// Konstanten //
+// *** Konstanten *** //
 const   // Erkennungsmuster Koordinatenpaar (mit Komma und/oder Leerzeichen getrennt)
 	REGEX_KOO = '/([0-9]{7,8}\.[0-9]{3,4})\s*,?\s*([0-9]{7}\.[0-9]{3,4})/',
 	REGEX_ARC = '/1e\+10([12])\s*,?\s*(-?[0-9]+\.[0-9]{3,4})/i';   // Radius; rechter/linker Bogen (1e+101/1e+102)
@@ -7,7 +8,7 @@ const   // Erkennungsmuster Koordinatenpaar (mit Komma und/oder Leerzeichen getr
 function koo(array $punkt):string {           // auf 3 Nachkommastellen gerundete Koordinatenpaare für gml:posList
     return sprintf('%.3f %.3f', $punkt['rechts'], $punkt['hoch']);
 }
-// Funktionen //
+//***  Funktionen *** //
 function bogenmitte(array $A,array $E):string{// Bogenmittenpunkt berechnen und ausgeben
     $D_h = $E['hoch']-$A['hoch'];             // Differenzvektor von ...
 	$D_r = $E['rechts']-$A['rechts'];         // ... A(nfangs-) zu E(ndpunkt)
@@ -15,15 +16,15 @@ function bogenmitte(array $A,array $E):string{// Bogenmittenpunkt berechnen und 
     if (abs($A['radius']) < $sek2) return ''; // Test auf ungültige Geometrie (ohne: $sek == 0 → Sonderfall Kreis) 
     $M_h = ($A['hoch']+$E['hoch'])/2;         // M(ittelpunkt) ...
 	$M_r = ($A['rechts']+$E['rechts'])/2;     // ... der Sekante AE
-	$lot = sqrt(pow($A['radius'],2)-pow($sek2,2));  // orthogonaler Abstand Kreismitte
-	$pfeil = abs($A['radius']-$lot);          // Pfeilhöhe (negativ bei überspanntem Bogen)
+	$lot = sqrt(pow($A['radius'],2)-pow($sek2,2));  // Orthogonalabstand Kreismitte
+	$pfeil = abs($A['radius']-$lot);          // Pfeilhöhe (negativ bei überspanntem Bogen > 200 gon)
     $N_r = -$D_h/$sek; $N_h = $D_r/$sek;      // Einheits-Normalvektor
     return koo(['rechts' => $M_r+($pfeil*$N_r*$A['dir']),'hoch' => $M_h+($pfeil*$N_h*$A['dir'])]);
 }
 function xmlsafe($wert,$default=''):string {  // XML-Sicherheit & Standardwerte
     return htmlspecialchars(trim($wert ?? $default),ENT_XML1|ENT_QUOTES,'UTF-8');
 }
-// Formular einlesen //
+//***  Formular einlesen *** //
 $antrag = xmlsafe($_POST['antrag'],'0000000000');
 $ubab = xmlsafe($_POST['ubab'],'0000');
 $name = xmlsafe($_POST['name'],'Baulast');
@@ -31,24 +32,21 @@ $bez = xmlsafe($_POST['bez']);
 $date = xmlsafe($_POST['date'] ?? '');
 $umring = [];
 if (!empty($_POST['umring'])) {
-    foreach (preg_split('/\R/', $_POST['umring']) as $row) {    //in Zeilen zerlegen
-        // Koordinatenpaare erkennen (Trenner: Leerzeichen und/oder Komma)
-        if (preg_match(REGEX_KOO, $row, $match)) {
-            $umring[] = ['hoch' => (float)$match[2], // als Dezimalzahl (zum rechnen & runden)
+    foreach (array_reverse(preg_split('/\R/',$_POST['umring'])) as $row) { // in Zeilen zerlegen; von hinten wegen umgedrehter Laufrichtung in GML
+        if (preg_match(REGEX_KOO, $row, $match)) {               // Koordinatenpaare erkennen (Trenner: Leerzeichen und/oder Komma)
+            $umring[] = ['hoch' => (float)$match[2],             // als Dezimalzahl (zum rechnen & runden)
                        'rechts' => (float)substr($match[1],strpos($match[1],'.')-6)]; // 6-stellig ohne UTM-Zonennummer
-        }	
-        elseif (preg_match(REGEX_ARC, $row, $match)) {           // Bogenradius und -richtung erkennen, aber
+        } elseif (preg_match(REGEX_ARC, $row, $match)) {         // Bogenradius und -richtung erkennen, aber
 			if (($last = array_key_last($umring)) !== null)      // nur wenn Array schon mit einem Koordinatenpaar befüllt ist,
 				$umring[$last] += ['radius' => (float)$match[2], // dann letzten Arrayeintrag um Radius ergänzen und  
-                                   'dir' => 3-2*(int)$match[1]]; // Krümmungsrichtung rechts (1 aus 1e+101) → +1 bzw. links (2 aus 1e+102) → -1
+                                   'dir' => 2*(int)$match[1]-3]; // inverse Krümmungsrichtung 1 (aus 1e+101) → -1 bzw. 2 (aus 1e+102) → +1
 		}
     }
-}
-// NAS-Ausgabe //
-header('Content-type: application/xml; charset=utf-8'); // Datei speichern
-header('Content-Disposition: attachment; filename="vFE_'.preg_replace('/[^A-Za-z0-9_-]/','',$antrag).'_001.xml"');   //Header-Injection absichern
-echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
-echo '<!-- bdvi-bb.de/baulast/ -->'."\n";
+} // *** NAS-Ausgabe *** //
+header('Content-type: application/xml; charset=utf-8');          // Datei speichern
+$safe = preg_replace('/[^A-Za-z0-9_-]/','',$antrag);             // Header-Injection absichern
+header('Content-Disposition: attachment; filename="vFE_'.$safe.'_001.xml"; filename*=UTF-8\'\''.rawurlencode("vFE_{$safe}_001.xml"));
+echo '<?xml version="1.0" encoding="UTF-8"?>'."\n".'<!-- erzeugt mit bdvi-bb.de/baulast/ -->'."\n";
 // NAS-Vorspann direkt ausgeben ?>
 <AX_Fortfuehrungsauftrag xmlns="http://www.adv-online.de/namespaces/adv/gid/7.1" xmlns:adv="http://www.adv-online.de/namespaces/adv/gid/7.1" xmlns:gco="http://www.isotc211.org/2005/gco" xmlns:gmd="http://www.isotc211.org/2005/gmd" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:ogc="http://www.adv-online.de/namespaces/adv/gid/ogc" xmlns:fes="http://www.opengis.net/fes/2.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:wfs="http://www.opengis.net/wfs/2.0" xmlns:wfsext="http://www.adv-online.de/namespaces/adv/gid/wfsext/2.0" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.adv-online.de/namespaces/adv/gid/7.1 NAS-Operationen.xsd">
   <empfaenger>
